05d7ef3896de56a16a90f73865802413929ce0d6
Leonard Adjei <leonardgre@gmail.com>
2025-10-06 20:51:28 -0500

    updated projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -10,7 +10,7 @@ export function Projects() {
         titleComponent={
           <>
             <h1 className="text-4xl font-semibold  text-white">
-        
+
               <span className="text-4xl md:text-[6rem] font-bold my-1 leading-none">
                 projects
               </span>
@@ -23,6 +23,32 @@ export function Projects() {
 }
 
 export const users = [
+      {
+    name: " ",
+    designation: "Braso-designs ecommerce - (might take a while for backend server to wake)",
+    image: "/projectimage13.png",
+    badge: "Github",
+    link: "https://brasodesigns.vercel.app/home",
+    github: ""
+  },
+    {
+    name: " ",
+    designation: "Quickmart Sales app",
+    image: "/projectimage12.png",
+    badge: "Github",
+    link: "https://quickmart-leo.vercel.app/",
+    github: ""
+  },
+
+  {
+    name: " ",
+    designation: "Shopperhunt Business",
+    image: "/proejctimage10.png",
+    badge: "Github",
+    link: "https://devapp.shopperhunt.com/",
+    github: ""
+  },
+
   {
     name: " ",
     designation: "Ndc good governance card",
@@ -31,61 +57,77 @@ export const users = [
     link: "https://www.ndcgoodgovernancecard.com",
     github: ""
   },
-  {
+    {
     name: " ",
-    designation: "Freetown rentals",
-    image: "/projectimage2.png",
+    designation: "Shopperhunt website",
+    image: "/projectimage9.png",
     badge: "Github",
-    link: "https://freetownrentals.com/",
-    github: "https://github.com/blameleo/fuserentals"
+    link: "https://devsite.shopperhunt.com/",
+    github: ""
   },
-  {
+      {
     name: " ",
-    designation: "Beehouse job search app",
-    image: "/projectimage3.png",
+    designation: "Eganow website",
+    image: "/projectimage11.png",
     badge: "Github",
-    link: "https://beehouse-app-frontend.vercel.app",
-    github: "https://github.com/blameleo/beehouse-app"
+    link: "https://eganow-website-test.vercel.app/",
+    github: ""
   },
-  {
-    name: " ",
-    designation: "Future ocean leaders academy",
-    image: "/projectimage4.png",
-    badge: "Github",
-    link: "https://ghana-ocean-literacy-academy.vercel.app/",
-    github: "https://github.com/blameleo/Ghana-Ocean-Literacy-Academy-"
-  },
-  {
-    name: " ",
-    designation: "Calendly clone",
-    image: "/projectimage5.png",
-    badge: "Github",
-    link: "https://magnificent-bubblegum-266b76.netlify.app/",
-    github: "https://github.com/blameleo/calendly"
-  },
-  {
-    name: " ",
-    designation: "Dictionary app",
-    image: "/projectimage6.png",
-    badge: "Github",
-    link: "https://dictionary-app-two-ashy.vercel.app/",
-    github: "https://github.com/blameleo/dictionary-app"
-  },
-  {
-    name: " ",
-    designation: "Countries app",
-    image: "/projectimage7.png",
-    badge: "Github",
-    link: "https://countries-api-app-sooty.vercel.app/",
-    github: "https://github.com/blameleo/countries_api_app"
-  },
-  {
-    name: " ",
-    designation: "Devjobs app",
-    image: "/projectimage8.png",
-    badge: "Github",
-    link: "https://devjobsleo.netlify.app/",
-    github: "https://github.com/blameleo/devjob-app"
-  },
-  
+  // {
+  //   name: " ",
+  //   designation: "Freetown rentals",
+  //   image: "/projectimage2.png",
+  //   badge: "Github",
+  //   link: "https://freetownrentals.com/",
+  //   github: "https://github.com/blameleo/fuserentals"
+  // },
+  // {
+  //   name: " ",
+  //   designation: "Beehouse job search app",
+  //   image: "/projectimage3.png",
+  //   badge: "Github",
+  //   link: "https://beehouse-app-frontend.vercel.app",
+  //   github: "https://github.com/blameleo/beehouse-app"
+  // },
+  // {
+  //   name: " ",
+  //   designation: "Future ocean leaders academy",
+  //   image: "/projectimage4.png",
+  //   badge: "Github",
+  //   link: "https://ghana-ocean-literacy-academy.vercel.app/",
+  //   github: "https://github.com/blameleo/Ghana-Ocean-Literacy-Academy-"
+  // },
+  // {
+  //   name: " ",
+  //   designation: "Calendly clone",
+  //   image: "/projectimage5.png",
+  //   badge: "Github",
+  //   link: "https://magnificent-bubblegum-266b76.netlify.app/",
+  //   github: "https://github.com/blameleo/calendly"
+  // },
+  // {
+  //   name: " ",
+  //   designation: "Dictionary app",
+  //   image: "/projectimage6.png",
+  //   badge: "Github",
+  //   link: "https://dictionary-app-two-ashy.vercel.app/",
+  //   github: "https://github.com/blameleo/dictionary-app"
+  // },
+  // {
+  //   name: " ",
+  //   designation: "Countries app",
+  //   image: "/projectimage7.png",
+  //   badge: "Github",
+  //   link: "https://countries-api-app-sooty.vercel.app/",
+  //   github: "https://github.com/blameleo/countries_api_app"
+  // },
+  // {
+  //   name: " ",
+  //   designation: "Devjobs app",
+  //   image: "/projectimage8.png",
+  //   badge: "Github",
+  //   link: "https://devjobsleo.netlify.app/",
+  //   github: "https://github.com/blameleo/devjob-app"
+  // },
+
 ];
